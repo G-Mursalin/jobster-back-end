f9231d87b34609e8d321f9eb2b2c24bbde8a64de
Golam Mursalin <golammursalin309@gmail.com>
2024-04-30 12:07:22 +0600

    Fix CORS error

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,13 @@ const app: Application = express();
 
 // Parser
 app.use(express.json());
-app.use(cors({ origin: ['*'], credentials: true }));
+app.use(
+    cors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        // credentials: true,
+    }),
+);
 app.use(cookieParser());
 
 // App route
