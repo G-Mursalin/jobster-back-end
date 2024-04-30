a21db25c0de65c92849bcc4005b6e48141f2eadb
Golam Mursalin <golammursalin309@gmail.com>
2024-04-30 12:21:06 +0600

    Add frontend local host URL to fix CORS error

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: '*',
+        origin: ['http://localhost:5173'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         // credentials: true,
     }),
